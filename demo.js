63c4c0d2df7110ac29d9a9422c7b039846b22815
Evgeny Ponomarev <evgeny@fluence.one>
2020-05-08 10:53:59 +0300

    js

---
+++ demo.js
@@ -0,0 +1,26 @@
+document.getElementById('addFile').addEventListener("click", toggleAddFile);
+document.getElementById('changeRelay').addEventListener("click", toggleChangeRelay);
+
+var relays = document.getElementsByClassName("chooseRelay");
+for (var i = 0; i < relays.length; i++) {
+    relays[i].addEventListener('click', chooseChangeRelay);
+}
+
+function toggleAddFile() {
+    section = document.getElementById('addFileSection');
+    if(section.style.display == 'none')
+      section.style.display = 'block'
+    else
+      section.style.display = 'none'
+}
+function toggleChangeRelay() {
+    section = document.getElementById('changeRelaySection');
+    if(section.style.display == 'none')
+      section.style.display = 'block'
+    else
+      section.style.display = 'none'
+}
+function chooseChangeRelay(event) {
+  document.getElementById('relayId').innerText = event.target.innerText;
+  document.getElementById('changeRelaySection').style.display = 'none';
+}
