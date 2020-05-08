d33e2685d5aa6af57401cc639deac5e288ac5c1d
Evgeny Ponomarev <evgeny@fluence.one>
2020-05-08 10:57:55 +0300

    js2

--- demo.js
+++ demo.js
@@ -1,5 +1,6 @@
 document.getElementById('addFile').addEventListener("click", toggleAddFile);
 document.getElementById('changeRelay').addEventListener("click", toggleChangeRelay);
+document.getElementById('toggleLogs').addEventListener("click", toggleLogs);
 
 var relays = document.getElementsByClassName("chooseRelay");
 for (var i = 0; i < relays.length; i++) {
@@ -15,6 +16,13 @@ function toggleAddFile() {
 }
 function toggleChangeRelay() {
     section = document.getElementById('changeRelaySection');
+    if(section.style.display == 'none')
+      section.style.display = 'block'
+    else
+      section.style.display = 'none'
+}
+function toggleLogs() {
+    section = document.getElementById('logs');
     if(section.style.display == 'none')
       section.style.display = 'block'
     else
